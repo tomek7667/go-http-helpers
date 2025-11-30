8ad1a9b900dd2ce570102f6495469471abf92714
tomek7667 <git@cyber-man.pl>
2025-11-30 23:47:46 +0100

    add mimetype to readfile

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tomek7667/go-http-helpers
 go 1.25.4
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.11
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.2.3
 	github.com/go-chi/cors v1.2.2
--- utils/file.go
+++ utils/file.go
@@ -3,20 +3,29 @@ package utils
 import (
 	"fmt"
 	"os"
+
+	"github.com/gabriel-vasile/mimetype"
 )
 
-func ReadFile(path string) (string, error) {
-	b, err := os.ReadFile(path)
-	if err != nil {
-		return "", fmt.Errorf("failed to read file at '%s': %w", path, err)
-	}
-	return string(b), nil
+func GetMimetype(b []byte) string {
+	mt := mimetype.Detect(b)
+	return mt.String()
 }
 
-func MustReadFile(path string) string {
-	s, err := ReadFile(path)
+// returns contents, mimetype with charset, error
+func ReadFile(path string) (string, string, error) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to read file at '%s': %w", path, err)
+	}
+	return string(b), GetMimetype(b), nil
+}
+
+// returns contents, mimetype
+func MustReadFile(path string) (string, string) {
+	s, mt, err := ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
-	return s
+	return s, mt
 }
