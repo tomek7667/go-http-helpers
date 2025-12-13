81b8d3a91c8e5ab67d1204997ce4b24f27c387a7
tomek7667 <git@cyber-man.pl>
2025-12-13 01:32:02 +0100

    refactor GetStringResponse for improved error handling and readability

--- h/getstringresponse.go
+++ h/getstringresponse.go
@@ -1,21 +1,30 @@
 package h
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 )
 
-func GetStringResponse(response *http.Response) (string, error) {
-	b, err := io.ReadAll(response.Body)
-	if err != nil {
-		return "", fmt.Errorf("failed to io.ReadAll: %w", err)
+func GetStringResponse(resp *http.Response) (string, error) {
+	if resp == nil || resp.Body == nil {
+		return "", nil
 	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("io.ReadAll: %w", err)
+	}
+
+	_ = resp.Body.Close()
+	resp.Body = io.NopCloser(bytes.NewReader(b))
+
 	return string(b), nil
 }
 
-func MustGetStringResponse(response *http.Response) string {
-	s, err := GetStringResponse(response)
+func MustGetStringResponse(resp *http.Response) string {
+	s, err := GetStringResponse(resp)
 	if err != nil {
 		panic(err)
 	}
