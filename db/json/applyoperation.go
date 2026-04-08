b58a00415fe5dbd90cfbd3f90ef92066a8dfd9e6
tomek7667 <git@cyber-man.pl>
2026-04-09 00:49:16 +0200

    error returned

--- db/json/applyoperation.go
+++ db/json/applyoperation.go
@@ -1,8 +1,13 @@
 package json
 
-func (c *Client[Db]) ApplyOperation(f func(db *Db) *Db) {
+func (c *Client[Db]) ApplyOperation(f func(db *Db) (*Db, error)) error {
 	c.m.Lock()
 	defer c.m.Unlock()
-	c.db = *f(&c.db)
+	db, err := f(&c.db)
+	if err != nil {
+		return err
+	}
+	c.db = *db
 	c.autosave()
+	return nil
 }
